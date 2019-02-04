1f41ce44e0e545d76316c59c8c23f8471b0b8d79
Sharad Sapre <40269852+sharadsapre22@users.noreply.github.com>
2019-02-04 11:43:27 +0530

    Line added in master branch through GitHub.

--- divide.js
+++ divide.js
@@ -1,4 +1,6 @@
 // Returns division of 2 numbers
 function Divide(a, b){
+    // 1. Line added in master branch through GitHub.
     return a/b;
 }
+// 2. Line added in master branch through GitHub.
