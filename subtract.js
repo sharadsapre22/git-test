d1fca4cbe04794f2a897148b75c690a1fe3a890d
Sharad Sapre <40269852+sharadsapre22@users.noreply.github.com>
2019-02-04 11:26:06 +0530

    Line added in master branch through Github.

--- subtract.js
+++ subtract.js
@@ -1,5 +1,7 @@
 // Returns subtraction of two numbers
 function Subtract(a, b) {
+    // 1. Line added in master branch through Github.
     return a - b;
     // Subtract function added
 }
+// 2. Line added in master branch through Github.
